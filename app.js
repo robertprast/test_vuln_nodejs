1bb163b2455a89a86de709b17a91b8bff8bc0612
Robert Prast <rprast@newrelic.com>
2021-02-04 17:34:32 -0500

    a

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("/test_above", function (req, res) {
+app.get("/test", function (req, res) {
     let str = "<html>test " + req.query.test;
     res.send(str);
   });
