7b2264c69d9f2f5b6c705fe5ddd0e530486393d1
Robert Prast <rprast@newrelic.com>
2021-02-04 17:27:54 -0500

    asdasd

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 var express = require("express");
 var app = express();
 
-app.get("/test", function (req, res) {
-  let test = [req.query.test];
-  res.send(eval(test[0]));
-});
-
-app.get("/", function (req, res) {
-    let str = "<html>test " + req.query.test;
-    res.send(str);
-  });
-  
-
 app.get("/2", function (req, res) {
   let str = "<html>test " + req.query.test;
   res.send(str);
 });
 
 
-function random(a){
-    return btoa(a)
+app.get("/test", function (req, res) {
+  let test = [req.query.test];
+  res.send(eval(test[0]));
+});
+
+app.get("/", function (req, res) {
+  let str = "<html>test " + req.query.test;
+  res.send(str);
+});
+
+function random(a) {
+  return btoa(a);
 }
 
 app.get("/3", function (req, res) {
-    let str = "<html>test " + random(req.query.test);
-    res.send(str);
-  });
+  let str = "<html>test " + random(req.query.test);
+  res.send(str);
+});
 
 app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
