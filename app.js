1df371babfa84f20f15e9bf6f595088c8a9c8246
Robert Prast <rprast@newrelic.com>
2021-02-04 17:18:51 -0500

    a

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send(eval(req.query.test));
+app.get("/", function (req, res) {
+  let test = [req.query.tes];
+  res.send(eval(test[0]));
   console.log(req.query.test);
-//   res.send("HI")
+  //   res.send("HI")
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log("Example app listening on port 8080!");
 });
