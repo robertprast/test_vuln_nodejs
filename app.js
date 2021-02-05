42ef2484eedc8b892fb294464d3feae0e5b30b02
Robert Prast <rprast@newrelic.com>
2021-02-05 09:49:24 -0500

    a

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ NodeJS test setup for testing SnykCode
 var express = require("express");
 var app = express();
 
+/************************************************/
 /*
 GENERAL TEST 1
 
@@ -30,11 +31,36 @@ SnykeCode Result:
 //   res.send(str);
 // });
 
+/************************************************/
 /*
 GENERAL TEST 2
 
 Code order : 
     Test 1 , Test 2 , Test 3
+SnykeCode Result: 
+    Test 1 -> XSS
+    Test 2 -> Nothing
+    Test 3 -> Code Injection
+*/
+
+// app.get("/test1", function (req, res) {
+//   let str = "<html>testasdas" + req.query.test;
+//   res.send(str);
+// });
+// app.get("/test2", function (req, res) {
+//   let str = "<html>test " + req.query.test;
+//   res.send(str);
+// });
+// app.get("/test3", function (req, res) {
+//   let test = [req.query.test];
+//   res.send(eval(test[0]));
+// });
+
+/************************************************/
+/* GENERAL TEST 3
+
+Code order : 
+    Test 2 , Test 3, Test 1 
 SnykeCode Result: 
     Test 1 -> XSS
     Test 2 -> Nothing
@@ -53,7 +79,6 @@ app.get("/test3", function (req, res) {
   let test = [req.query.test];
   res.send(eval(test[0]));
 });
-
 
 app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
