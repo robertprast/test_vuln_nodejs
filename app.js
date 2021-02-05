9bfcb384f86710ab162c740223fa492e1dc9e807
Robert Prast <rprast@newrelic.com>
2021-02-05 09:51:44 -0500

    a

--- app.js
+++ app.js
@@ -62,15 +62,10 @@ SnykeCode Result:
 Code order : 
     Test 2 , Test 3, Test 1 
 SnykeCode Result: 
-    Test 1 -> XSS
+    Test 1 -> Nothing
     Test 2 -> Nothing
-    Test 3 -> Code Injection
+    Test 3 -> XSS , Code Injection
 */
-
-app.get("/test1c", function (req, res) {
-  let str = "<html>testasdas" + req.query.test;
-  res.send(str);
-});
 app.get("/test2c", function (req, res) {
   let str = "<html>test " + req.query.test;
   res.send(str);
@@ -78,6 +73,10 @@ app.get("/test2c", function (req, res) {
 app.get("/test3c", function (req, res) {
   let test = [req.query.test];
   res.send(eval(test[0]));
+});
+app.get("/test1c", function (req, res) {
+  let str = "<html>testasdas" + req.query.test;
+  res.send(str);
 });
 
 app.listen(8080, function () {
